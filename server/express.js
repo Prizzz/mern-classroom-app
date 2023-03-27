6b2d797205fd1794d11ec542d156ec58973bba11
Prizzz <rob_243248@mail.ru>
2023-03-27 19:40:21 +0400

    feat: add Home page

--- server/express.js
+++ server/express.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
@@ -8,7 +9,14 @@ import Template from './../template';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes';
 
+//comment out before building for production
+import devBundle from './devBundle';
+
+const CURRENT_WORKING_DIR = process.cwd();
 const app = express();
+
+//comment out before building for production
+devBundle.compile(app);
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
@@ -16,9 +24,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 // secure apps by setting various HTTP headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors());
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 
 // mount routes
 app.use('/', userRoutes);
