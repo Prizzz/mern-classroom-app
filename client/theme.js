dd0986d61d34a87dfc2f58bb00ba8cc90b79d784
Prizzz <rob_243248@mail.ru>
2023-03-29 01:38:44 +0400

    fix: update package.json

--- client/theme.js
+++ client/theme.js
@@ -1,7 +1,7 @@
-import { createTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 import { pink } from '@material-ui/core/colors';
 
-const theme = createTheme({
+const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
   },
